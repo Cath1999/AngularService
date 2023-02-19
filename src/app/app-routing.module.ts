15486aec0385a5a7eb7436401818e5cfd30b0b87
Cath1999 <catherine.mena11751@est.uca.edu.ni>
2023-02-18 22:51:15 -0600

    ng g c components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './Persona/Add/add/add.component';
+import { EditComponent } from './Persona/Edit/edit/edit.component';
+import { ListarComponent } from './Persona/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //components routing
+{path:'listar', component:ListarComponent},
+{path:'add',component:AddComponent},
+{path:'edit', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/Add/add/add.component';
+import { EditComponent } from './Persona/Edit/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarComponent
+    ListarComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
